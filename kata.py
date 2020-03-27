c1e32e2803e268c91f083bd0e6b008b015271529
Garpur3 <garpurhnefill@gmail.com>
2020-03-27 19:21:52 +0000

    One negative works

--- kata.py
+++ kata.py
@@ -4,8 +4,13 @@ def Add(numbers):
     if len(numbers) > 0:
         list_of_numbers = re.split(",|\n", numbers)
         total = 0
+        negatives = ""
         for number in list_of_numbers:
             if int(number) <= 1000:
+                if int(number) < 0:
+                    negatives += number + " "
                 total += int(number)
+        if len(negatives) > 0:
+            raise ValueError(f"Negatives not allowed: {numbers}")
         return total
     return 0
