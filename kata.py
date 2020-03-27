45dd795f42338c230537972b0912683d4cfc3fe9
Garpur3 <garpurhnefill@gmail.com>
2020-03-27 19:07:54 +0000

    Works with newline

--- kata.py
+++ kata.py
@@ -1,6 +1,8 @@
+import re
+
 def Add(numbers):
     if len(numbers) > 0:
-        list_of_numbers = numbers.split(",")
+        list_of_numbers = re.split(",|\n", numbers)
         total = 0
         for number in list_of_numbers:
             total += int(number)
